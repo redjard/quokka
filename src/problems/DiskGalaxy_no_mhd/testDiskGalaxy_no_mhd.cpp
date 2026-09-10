9c0bf065353bdbe03b78f4097d8d872a77b083a5
redjard <github@redjard.org>
2026-09-10 09:34:46 +0200

    take addStrangSplitSources from lizmcole/MHDDisk and replace computeAfterTimestep entrypoint with addStrangSplitSources to properly input our own effects only

--- src/problems/DiskGalaxy_no_mhd/testDiskGalaxy_no_mhd.cpp
+++ src/problems/DiskGalaxy_no_mhd/testDiskGalaxy_no_mhd.cpp
@@ -510,47 +510,72 @@ template <> void QuokkaSimulation<DiskGalaxy_no_mhd>::refineGrid(int lev, amrex:
 	// amrex::Print() << "REDJARD: ran refineGrid in " << float(clock() - start)/1e6 << " s\n";
 }
 
-// // example: analytic halo potential acceleration
-// amrex::GpuArray<Real,3> grav_accel(Real x, Real y, Real z) {
-// 	// return (-dPhi/dx, -dPhi/dy, -dPhi/dz)
-// }
-void apply_dm_potential_on_grid( quokka::grid const &grid_elem ) {
-	const amrex::Box &indexRange = grid_elem.indexRange_;
+void apply_dm_potential_on_grid( quokka::grid const &grid_elem, amrex::Real dt ) {
+	// const amrex::Box &indexRange = grid_elem.indexRange_;
 	const amrex::GpuArray<amrex::Real, AMREX_SPACEDIM> dx = grid_elem.dx_;
 	const amrex::GpuArray<amrex::Real, AMREX_SPACEDIM> prob_lo = grid_elem.prob_lo_;
-	// const amrex::Array4<double> &state_cc = grid_elem.array_;
+	// const amrex::Array4<double> &state = grid_elem.array_;
 	
-	amrex::Print() << "REDJARD: level = " << level << "\n";
-	amrex::Print() << "REDJARD: iter.index() = " << iter.index() << "\n";
+	// amrex::Print() << "REDJARD: level = " << level << "\n";
+	// amrex::Print() << "REDJARD: iter.index() = " << iter.index() << "\n";
 	amrex::Print() << "REDJARD: prob_lo = [" << prob_lo[0] << ", " << prob_lo[1] << ", " << prob_lo[2] << "]\n";
 	amrex::Print() << "REDJARD: dx = [" << dx[0] << ", " << dx[1] << ", " << dx[2] << "]\n";
+	
+	/*
+	// taken from lizmcole/MHDDisk
+	amrex::ParallelFor(indexRange, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+		const double x = prob_lo[0] + (i + 0.5) * dx[0];
+		const double y = prob_lo[1] + (j + 0.5) * dx[1];
+		const double z = prob_lo[2] + (k + 0.5) * dx[2];
+		const double R2 = x * x + y * y;
+		const double R = std::sqrt(R2);
+		
+		const double rho = state(i, j, k, HydroSystem<MHDGalaxy>::density_index);
+		const double px = state(i, j, k, HydroSystem<MHDGalaxy>::x1Momentum_index);
+		const double py = state(i, j, k, HydroSystem<MHDGalaxy>::x2Momentum_index);
+		const double pz = state(i, j, k, HydroSystem<MHDGalaxy>::x3Momentum_index);
+		const double Eint = state(i, j, k, HydroSystem<MHDGalaxy>::internalEnergy_index);
+		const double Etot_old = state(i, j, k, HydroSystem<MHDGalaxy>::energy_index);
+		const double Ekin_old = 0.5 * (px * px + py * py + pz * pz) / rho;
+		const double Emag = Etot_old - Ekin_old - Eint;
+		
+		const double D = R2 + Rc * Rc + (z / q_flatten) * (z / q_flatten);
+		const double g_R = (R > 0.0) ? -(vc * vc * R / D) : 0.0;
+		const double g_z = -(vc * vc * z / (q_flatten * q_flatten * D));
+		const double gx = (R > 0.0) ? g_R * x / R : 0.0;
+		const double gy = (R > 0.0) ? g_R * y / R : 0.0;
+		
+		const double px_new = px + dt_lev * rho * gx;
+		const double py_new = py + dt_lev * rho * gy;
+		const double pz_new = pz + dt_lev * rho * g_z;
+		const double Ekin_new = 0.5 * (px_new * px_new + py_new * py_new + pz_new * pz_new) / rho;
+		
+		state(i, j, k, HydroSystem<MHDGalaxy>::x1Momentum_index) = px_new;
+		state(i, j, k, HydroSystem<MHDGalaxy>::x2Momentum_index) = py_new;
+		state(i, j, k, HydroSystem<MHDGalaxy>::x3Momentum_index) = pz_new;
+		state(i, j, k, HydroSystem<MHDGalaxy>::energy_index) = Ekin_new + Eint + Emag;
+	});
+	//*/
 }
 
-void apply_dm_potential( QuokkaSimulation<DiskGalaxy_no_mhd>*const sim ) {
-	for (int level = 0; level <= sim->finestLevel(); ++level) {
-		// taken from simulation.hpp setInitialConditionsAtLevel_cc
-		for (amrex::MFIter iter(sim->state_new_cc_[level]); iter.isValid(); ++iter) {
-			quokka::grid grid_elem(
-				sim->state_new_cc_[level].array(iter),
-				iter.validbox(),
-				sim->geom[level].CellSizeArray(),
-				sim->geom[level].ProbLoArray(),
-				sim->geom[level].ProbHiArray(),
-				quokka::centering::cc,
-				quokka::direction::na
-			);
-			apply_dm_potential_on_grid(grid_elem);
-		}
+template <> void QuokkaSimulation<DiskGalaxy_no_mhd>::addStrangSplitSources(amrex::MultiFab &mf, int level, amrex::Real /*time*/, amrex::Real dt_lev) {
+	// iterate over all grids (of this MultiFab)
+	for (amrex::MFIter iter(mf); iter.isValid(); ++iter) {
+		quokka::grid grid_elem(
+			mf.array(iter),
+			iter.validbox(),
+			this->geom[level].CellSizeArray(),
+			this->geom[level].ProbLoArray(),
+			this->geom[level].ProbHiArray(),
+			quokka::centering::cc,
+			quokka::direction::na
+		);
+		
+		// dark matter gravitational potential
+		apply_dm_potential_on_grid(grid_elem,dt_lev);
+		
 	}
 }
-template <> void QuokkaSimulation<DiskGalaxy_no_mhd>::computeAfterTimestep() {
-	apply_dm_potential(this);
-}
-// template <> void QuokkaSimulation<DiskGalaxy_no_mhd>::calculateGpotAllLevels() {
-// 	amrex::Print() << "REDJARD: calling calculateGpotAllLevels\n";
-// 	((AMRSimulation<DiskGalaxy_no_mhd>*)this)->calculateGpotAllLevels();
-// 	amrex::Print() << "REDJARD: called calculateGpotAllLevels\n";
-// }
 
 auto problem_main() -> int
 {
